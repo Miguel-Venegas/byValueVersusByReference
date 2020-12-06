44be373892bac735f61bf4549d677a40064e30eb
Miguel_Venegas <MiguelVenegas@Miguels-MBP.routerlogin.net>
2020-12-06 07:17:43 -0800

    by reference even as parameters

--- app.js
+++ app.js
@@ -25,3 +25,27 @@ console.log(c); //output = {greeting: 'hello'}
 console.log(d); //output = {greeting: 'hello'}
 
 // this output was different than the by-value output example above. Unlike the above example, both variables point to the same object. That is, no new copy is created. Thus, if you mutate the object, it will affect any variable referencing it as well. In other words, they are pointing to the same location in memory.
+
+//by reference even as parameters
+
+function changeGreeting(obj){
+    obj.greeting = 'Hola'; //mutating
+}
+
+changeGreeting(d);
+
+console.log(c); // output {greeting: "Hola"}
+console.log(d); // output {greeting: "Hola"}
+
+// this output operated exactly like the above example.
+
+// equals operator sets up new memory space (new address)
+
+c = {greeting: 'howdy'};
+
+console.log(c);
+console.log(d);
+
+// this is a special case where the equals operator sets up a new memory location for the variable; thus, it is no longer pointing to the same object by reference.
+
+
